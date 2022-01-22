72b596295a6151e53695d40e821544f6b143a7d6
William <hnbaolam@gmail.com>
2022-01-23 00:11:43 +0700

    add gamemanager object and script

---
+++ 2D Tutorial Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    // References
+    public Player player;
+
+    // Logic
+    public int pesos;
+    public int experience;
+
+    // Save state
+    public void SaveState()
+    {
+        Debug.Log("SaveState");
+    }
+
+    public void LoadState()
+    {
+        Debug.Log("LoadState");
+    }
+}
